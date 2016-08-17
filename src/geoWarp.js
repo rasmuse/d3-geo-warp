e9d0879bcecdc0562eb838f7db2e1c777c643d6e
Rasmus Einarsson <mr@rasmuseinarsson.se>
2016-08-17 15:03:00 +0200

    Start rewriting for node-gdal

--- src/geoWarp.js
+++ src/geoWarp.js
@@ -1,131 +1,132 @@
-import {geoPath} from "d3-geo";
+import {geoPath, geoProjection} from "d3-geo";
 
-var NUM_COMPONENTS = 4; // RGBA
+var NUM_MAS = 4; // RGBA
 var SPHERE = {type: "Sphere"};
 
-function linearCoords(img, col, row) {
-    return (row * img.width + col) * NUM_COMPONENTS;
-}
-
-function setPixel(img, col, row, val) {
-    var ix0 = linearCoords(img, col, row);
-    val.forEach(function (component, di) {
-        img.data[ix0 + di] = component;
-    });
-}
-
 function clamp(val, min, max) {
-    if (min > max) {
-        throw 'min > max'
-    }
-    return Math.max(min, Math.min(val, max));
+  if (min > max) {
+    throw 'min > max'
+  }
+  return Math.max(min, Math.min(val, max));
 }
 
-function getPixel(imageData, col, row) {
-    var ix0 = linearCoords(imageData, col, row);
-    return imageData.data.slice(ix0, ix0 + NUM_COMPONENTS);
-}
-
-function isAlphaZero(imageData, col, row) {
-    return (imageData.data[linearCoords(imageData, col, row) + 3] == 0);
-}
-
-function interpolate(imageData, point) {
-    var x = point[0];
-    var y = point[1];
-    var col = clamp(Math.round(x), 0, imageData.width-1);
-    var row = clamp(Math.round(y), 0, imageData.height-1);
-    return getPixel(imageData, col, row);
+function interpolate(src, point) {
+  var x = point[0];
+  var y = point[1];
+  var col = clamp(Math.round(x), 0, imageData.width-1);
+  var row = clamp(Math.round(y), 0, imageData.height-1);
+  return src.get(col, row);
 }
 
 export default function() {
-    var srcProj, dstProj,
-        dstContext, dstCanvas,
-        maskObject;
+  var srcProj = geoProjection(null),
+      dstProj = geoProjection(null),
+      maskObject = null,
+      chunkSize = [1000, 1000],
+      createCanvas,
+      maskCanvas,
+      maskContext,
+      src,
+      dst;
 
-    function createCanvas(width, height) {
-        var canvas = document.createElement('canvas');
-        if (typeof width !== 'undefined') canvas.width = width;
-        if (typeof height !== 'undefined') canvas.height = height;
-        return canvas;
+  function getMask(x0, y0, x1, y1) {
+    var width = x1 - x0;
+    var height = y1 - y0;
+
+    // Temporarily translate the dstProjection
+    var translate0 = dstProj.translate();
+    dstProj.translate([translate0[0] - x0, translate0[1] - y0]);
+    
+    maskContext.clearRect(0, 0, width, height);
+    maskContext.beginPath();
+    geoPath().projection(dstProj).context(context)(maskObject);
+    maskContext.closePath();
+    maskContext.fill();
+    
+    dstProj.translate(translate0);
+    
+    var m = maskContext.getImageData(0, 0, width, height).data;
+
+    return function(x, y) {
+      // To mask image coords
+      x = x - x0;
+      y = y - y0;
+
+      // Get first component of mask pixel
+      var linearCoords = (y * width + x) * NUM_MAS;
+      return m[linearCoords] != 0;
     }
+  }
 
-    function warp(srcImage) {
-        var srcCanvas = createCanvas(srcImage.width, srcImage.height),
-            srcContext = srcCanvas.getContext('2d'),
-            maskData = makeMask();
+  function warpChunk(x0, y0, x1, y1) {
+    var isVisible = makeMask(x0, y0, x1, y1);
+    var dstPoint, srcPoint;
 
-        srcContext.drawImage(srcImage, 0, 0);
-        srcImage = srcContext.getImageData(
-            0, 0, srcCanvas.width, srcCanvas.height);
-
-        var inverseProjection = function (point) {
-            return srcProj(dstProj.invert(point));
+    for (var x = x0; x < x1; x++) {
+      for (var y = y0; y < y1; y++) {
+        if (isVisible(x, y)) {
+          // Invert middle of pixel coordinate
+          dstPoint = [x + 0.5, y + 0.5];
+          srcPoint = srcProj(dstProj.invert(point));
+          dst.set(dstPoint[0], dstPoint[1], interpolate(src, srcPoint));
         }
+      }
+    }
+  }
 
-        var dstImage = dstContext.getImageData(0, 0, dstCanvas.width, dstCanvas.height);
+  function warp(bbox) {
 
-        for (var row = 0; row < dstImage.height; row++) {
-            for (var col = 0; col < dstImage.width; col++) {
-                if (isAlphaZero(maskData, col, row)) continue;
+    maskCanvas = createCanvas(chunkSize[0], chunkSize[1]);
+    maskContext = maskCanvas.getContext('2d');
+    maskContext.fillStyle = '#fff';
 
-                var projectedPoint = [col+0.5, row+0.5],
-                    inversePoint = inverseProjection(projectedPoint);
-                setPixel(dstImage, col, row, interpolate(srcImage, inversePoint));
-            }
-        }
-
-        dstContext.putImageData(dstImage, 0, 0);
+    if (typeof(bbox) === 'undefined') {
+      bbox = {
+        x0: 0,
+        x1: dst.width - 1,
+        y0: 0,
+        y1: dst.height - 1
+      };
     }
 
-    warp.dstProj = function(_) {
-        if (!arguments.length) return dstProj;
-        dstProj = _;
-        return warp;
-    };
-
-    warp.srcProj = function(_) {
-        if (!arguments.length) return srcProj;
-        srcProj = _;
-        return warp;
-    };
-
-    warp.dstContext = function(_) {
-        if (!arguments.length) return dstContext;
-        dstContext = _;
-        dstCanvas = dstContext.canvas;
-        return warp;
-    };
-
-    function makeMask() {
-        var width = dstCanvas.width,
-            height = dstCanvas.height,
-            maskCanvas = createCanvas(),
-            context = maskCanvas.getContext('2d');
-
-        maskCanvas.width = width;
-        maskCanvas.height = height;
-
-        context.beginPath();
-        context.fillStyle = '#fff';
-        geoPath().projection(dstProj).context(context)(maskObject);
-        context.closePath();
-        context.fill();
-
-        return context.getImageData(0, 0, width, height);
+    for (var x = bbox.x0; x <= x1; x += chunkSize[0]) {
+      for (var y = bbox.y0; y <= y1; y += chunkSize[1]) {
+        warpChunk(
+          x,
+          y,
+          x + Math.min(x1, x + chunkSize[0]),
+          y + Math.min(y1, y + chunkSize[1]));
+      }
     }
+  }
 
-    warp.maskObject = function (_) {
-        if (!arguments.length) return maskObject;
-        maskObject = _;
-        return warp;
-    }
+  warp.dstProj = function(_) {
+    return arguments.length ? (dstProj = _, warp) : dstProj;
+  };
 
-    warp.createCanvas = function (_) {
-        if (!arguments.length) return createCanvas;
-        createCanvas = _;
-        return warp;
-    }
+  warp.srcProj = function(_) {
+    return arguments.length ? (srcProj = _, warp) : srcProj;
+  };
 
-    return warp.maskObject(SPHERE);
+  warp.dst = function(_) {
+    return arguments.length ? (dst = _, warp) : dst;
+  };
+
+  warp.src = function(_) {
+    return arguments.length ? (src = _, warp) : src;
+  };
+
+  warp.chunkSize = function(_) {
+    return arguments.length ? (chunkSize = _, warp) : chunkSize;
+  };
+
+  warp.maskObject = function(_) {
+    return arguments.length ? (maskObject = _, warp) : maskObject;
+  };
+
+  warp.createCanvas = function(_) {
+    return arguments.length ? (createCanvas = _, warp) : createCanvas;
+  };
+
+  return warp.maskObject(SPHERE);
 }
